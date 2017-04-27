1143de71d1863e610c860bcf64e51590825960b5
chasetoy <chase.a.toy@gmail.com>
2017-04-27 19:21:55 -0400

    updated authors names [typos]

--- CSC420/src/csc420/Main.java
+++ CSC420/src/csc420/Main.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2017 Laura Hunt, Chase Toy, Cameron Everett.
+ * Copyright 2017 Laura Buckman, Chase Toy, Cameron Everett.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
